10a4b1e884128217f9f49e7ebf0e70194b98b106
Rofiq354 <rofik010206@gmail.com>
2026-03-10 15:14:35 +0700

    fix: cors credential secure

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -60,7 +60,7 @@ export const register = async (
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       // maxAge: 24 * 60 * 60 * 1000,
       maxAge: 3 * 60 * 60 * 1000,
@@ -131,7 +131,7 @@ export const login = async (
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
       // maxAge: 15 * 1000,
